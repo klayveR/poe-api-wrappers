4de5eb28b953f772c23cf4ce20621f35d037d898
klayveR <tobiashoffmann7@gmail.com>
2021-03-23 10:00:44 +0100

    feat: get ladder in league object

--- src/poe/apis/leagues/League.ts
+++ src/poe/apis/leagues/League.ts
@@ -2,8 +2,9 @@ import { Type } from "class-transformer";
 
 import { Transformable } from "../../../common/classes";
 import { Realm } from "../../shared/models";
-import { Ladder } from "../ladders";
+import { Ladder, Options } from "../ladders";
 import { Rule } from "./Rule";
+import * as API from "../ladders/API";
 
 export class League extends Transformable {
     id!: string;
@@ -29,4 +30,23 @@ export class League extends Transformable {
 
     @Type(/* istanbul ignore next */ () => Ladder)
     ladder?: Ladder;
+
+    /**
+     * @remarks
+     * If `store` is set to true and [[ladder]] had a ladder already, it will be overwritten.
+     *
+     * @param options
+     * @param store If set to `true`, the ladder will be saved in the [[ladder]] property
+     * @returns The ladder for this league
+     * @throws [[APIError]]
+     */
+    public async getLadder(options?: Options, store = true): Promise<Ladder> {
+        const ladder = await API.get(this.id, options);
+
+        if (store) {
+            this.ladder = ladder;
+        }
+
+        return ladder;
+    }
 }
